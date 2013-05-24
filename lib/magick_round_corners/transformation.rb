348be56de864238309ad9220648a5c97a8c0dd22
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2013-05-24 12:30:54 -0500

    Avoid generating the mask outside the bounded area

--- lib/magick_round_corners/transformation.rb
+++ lib/magick_round_corners/transformation.rb
@@ -9,7 +9,7 @@ module MagickRoundCorners
         # Create a white rectangle with rounded corners. This will become the
         # mask for the area you want to retain in the original image.
         ::Magick::Draw.new.stroke('none').stroke_width(0).fill('white').
-          roundrectangle(0, 0, columns, rows, radius, radius).
+          roundrectangle(0, 0, columns-1, rows-1, radius, radius).
           draw(mask)
         # Apply the mask and write it out
         composite(mask, ::Magick::CenterGravity, ::Magick::CopyOpacityCompositeOp)
